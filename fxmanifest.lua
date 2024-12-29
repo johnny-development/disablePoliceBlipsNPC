91fe63d2df95b56dc9ce0a09e90c0faf8768d6e6
Dingo's Development <bananarenan5@gmail.com>
2024-12-30 05:06:21 +1100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'DINGOS Development ::: Develooer: DINGO'
+description 'Hides AI Police blips from the minimap and main map.'
+version '1.0.0'
+
+client_script 'disablePoliceBlips.lua'
